7a309bc9ed3f34c801b89a27b704b2be014260e5
bgwolfe <bwolfe53144@yahoo.com>
2025-02-17 00:56:31 -0600

    update

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: 'caboose.proxy.rlwy.net:10166',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
