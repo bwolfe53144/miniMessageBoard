030297eb0781fbef028827685be52c40b98b5c72
bgwolfe <bwolfe53144@yahoo.com>
2025-02-17 16:31:19 -0600

    update

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+const connectionString =
+  "postgresql://postgres:ofdzHDfGpEKRdCbQuFrCOvSyZcEYuLde@tramway.proxy.rlwy.net:54203/railway";
+
+  const pool = new Pool({
+    connectionString,
+  });
+  
+  module.exports = pool;
