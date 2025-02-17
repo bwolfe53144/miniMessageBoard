feba72a45cb8a657f23cb27a595fd4d5cb65a730
bgwolfe <bwolfe53144@yahoo.com>
2025-02-17 09:35:43 -0600

    update

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
+  host: 'minimessageboard-production-50ad.up.railway.app',
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
