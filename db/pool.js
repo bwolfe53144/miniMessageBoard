50616a82833d393a9e0e3cb908366000d9a337af
bgwolfe <bwolfe53144@yahoo.com>
2025-02-17 10:26:01 -0600

    update

--- db/pool.js
+++ db/pool.js
@@ -5,6 +5,6 @@ module.exports = new Pool({
   host: 'postgres.railway.internal',
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
+  password: 'HvZSZAeoAOWPhSFJXzBwIPRwTszuvKUj',
   port: process.env.PGPORT,
 });
