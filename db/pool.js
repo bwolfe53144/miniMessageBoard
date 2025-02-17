b7020986a8bcc1b01354a1be4586eae06c76f070
bgwolfe <bwolfe53144@yahoo.com>
2025-02-16 22:55:08 -0600

    update

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
+  host: 'dabase',
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
